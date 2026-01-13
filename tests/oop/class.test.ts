fe691c33aba6c17b572e88eed626b5ceb0e88f6a
Ricky Andrianto K <ricky@codr.id>
2026-01-13 09:59:46 +0700

    Add: OOP Implementation Class on Typescript

---
+++ tests/oop/class.test.ts
@@ -0,0 +1,17 @@
+describe("Class", () => {
+  class Customer {
+    constructor() {
+      console.info("Create new Customer");
+    }
+  }
+  class Order {}
+  it("should can create class", () => {
+    const customer: Customer = new Customer();
+    const order = new Order();
+  });
+
+  it("should can create constructors", () => {
+    new Customer();
+    new Customer();
+  });
+});
