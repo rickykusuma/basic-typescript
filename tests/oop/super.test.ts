3c2812b567c9e720a9ace7e60b4c6d045b9d8062
Ricky Andrianto K <ricky.ad58@gmail.com>
2026-01-13 12:15:32 +0700

    Add: Basic OOP Implementation Super Constructor on TypeScript

---
+++ tests/oop/super.test.ts
@@ -0,0 +1,20 @@
+describe("Super Constructor", () => {
+  class Person {
+    name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+  class Employee extends Person {
+    department: string;
+    constructor(name: string, department: string) {
+      super(name);
+      this.department = department;
+    }
+  }
+
+  it("should support", () => {
+    const employee = new Employee("Ricky", "IT");
+    console.info(employee);
+  });
+});
